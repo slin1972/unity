5698ee654c9a0f8cea6e64212d6a1f23e6d7e6ce
slin1972 <lykin_1972@163.com>
2016-11-02 11:11:32 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+      name="unity",
+      version="0.10",
+      description="My unity module",
+      author="lesenlin",
+      url="https://slin1972.github.com",
+      license="LGPL",
+      packages=find_packages(),
+      scripts=["scripts/unity.py"],
+)
---
+++ unity.py
@@ -0,0 +1,5 @@
+import sys
+
+def get():
+
+    return sys.path
